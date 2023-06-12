f8f9dc10ab584eca6cfcd1227a836adb61c31d20
Sounak Kundu <sounak.kun@gmail.com>
2023-06-12 21:34:20 +0530

    Perf(07): replace hashmap with vec

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,104 +1,107 @@
-use std::collections::HashMap;
 use std::io::{self, prelude::*};
 
-struct Instruction<'a> {
-    operation: Operator<'a>,
-    output: &'a str,
+struct Instruction {
+    operation: Operator,
+    output: usize,
 }
 
-enum Operator<'a> {
-    NOP(Operand<'a>),
-    AND(Operand<'a>, Operand<'a>),
-    OR(Operand<'a>, Operand<'a>),
-    LSHIFT(Operand<'a>, Operand<'a>),
-    RSHIFT(Operand<'a>, Operand<'a>),
-    NOT(Operand<'a>),
+enum Operator {
+    NOP(Operand),
+    AND(Operand, Operand),
+    OR(Operand, Operand),
+    LSHIFT(Operand, Operand),
+    RSHIFT(Operand, Operand),
+    NOT(Operand),
 }
 
-enum Operand<'a> {
-    Wire(&'a str),
+enum Operand {
+    Wire(usize),
     Value(u16),
 }
 
-impl<'a> Operand<'a> {
-    fn new(input: &'a str) -> Self {
-        input.parse().map_or(Operand::Wire(input), Operand::Value)
+impl Operand {
+    fn new(input: &str) -> Self {
+        input.parse().map_or_else(
+            |_| Operand::Wire(Circuit::wire_index(input)),
+            Operand::Value,
+        )
     }
 }
 
-type WireState<'a> = Result<u16, &'a Instruction<'a>>;
+type WireState<'a> = Option<Result<u16, &'a Instruction>>;
 
 struct Circuit<'a> {
-    circuit: HashMap<&'a str, WireState<'a>>,
+    circuit: Vec<WireState<'a>>,
 }
 
 impl<'a> Circuit<'a> {
     fn new() -> Self {
         Self {
-            circuit: HashMap::new(),
+            // 26 * 26 (2 letter wires) + 26 (1 letter wires)
+            circuit: vec![None; 26 * 26 + 26],
         }
     }
 
-    fn run(&mut self, instruction: &'a Instruction<'a>) {
+    fn wire_index(wire: &str) -> usize {
+        debug_assert!(wire.len() <= 2, "{}", wire);
+        wire.bytes()
+            .fold(0, |acc, byte| (acc * 26) + (byte - b'a' + 1) as usize)
+    }
+
+    fn run(&mut self, instruction: &'a Instruction) {
         match &instruction.operation {
             Operator::NOP(a) | Operator::NOT(a) => {
                 let a = match a {
-                    Operand::Wire(wire) => self.get(wire),
+                    Operand::Wire(wire) => self.get(*wire),
                     Operand::Value(value) => Some(*value),
                 };
                 let Some(a) = a else {
-                    self.circuit.insert(instruction.output, Err(instruction));
+                    self.circuit[instruction.output] = Some(Err(instruction));
                     return;
                 };
-                self.circuit.insert(
-                    instruction.output,
-                    match instruction.operation {
-                        Operator::NOP(_) => Ok(a),
-                        Operator::NOT(_) => Ok(!a),
-                        _ => unreachable!(),
-                    },
-                );
+                self.circuit[instruction.output] = match instruction.operation {
+                    Operator::NOP(_) => Some(Ok(a)),
+                    Operator::NOT(_) => Some(Ok(!a)),
+                    _ => unreachable!(),
+                };
             }
             Operator::AND(a, b)
             | Operator::OR(a, b)
             | Operator::LSHIFT(a, b)
             | Operator::RSHIFT(a, b) => {
                 let a = match a {
-                    Operand::Wire(wire) => self.get(wire),
+                    Operand::Wire(wire) => self.get(*wire),
                     Operand::Value(value) => Some(*value),
                 };
                 let Some(a) = a else {
-                    self.circuit.insert(instruction.output, Err(instruction));
+                    self.circuit[instruction.output] = Some(Err(instruction));
                     return;
                 };
                 let b = match b {
-                    Operand::Wire(wire) => self.get(wire),
+                    Operand::Wire(wire) => self.get(*wire),
                     Operand::Value(value) => Some(*value),
                 };
                 let Some(b) = b else {
-                    self.circuit.insert(instruction.output, Err(instruction));
+                    self.circuit[instruction.output] = Some(Err(instruction));
                     return;
                 };
-                self.circuit.insert(
-                    instruction.output,
-                    match instruction.operation {
-                        Operator::AND(_, _) => Ok(a & b),
-                        Operator::OR(_, _) => Ok(a | b),
-                        Operator::LSHIFT(_, _) => Ok(a << b),
-                        Operator::RSHIFT(_, _) => Ok(a >> b),
-                        _ => unreachable!(),
-                    },
-                );
+                self.circuit[instruction.output] = match instruction.operation {
+                    Operator::AND(_, _) => Some(Ok(a & b)),
+                    Operator::OR(_, _) => Some(Ok(a | b)),
+                    Operator::LSHIFT(_, _) => Some(Ok(a << b)),
+                    Operator::RSHIFT(_, _) => Some(Ok(a >> b)),
+                    _ => unreachable!(),
+                };
             }
         }
     }
 
-    fn get(&mut self, wire: &'a str) -> Option<u16> {
-        match self.circuit.get(wire)? {
-            Ok(value) => Some(*value),
+    fn get(&mut self, wire: usize) -> Option<u16> {
+        match self.circuit[wire]? {
+            Ok(value) => Some(value),
             Err(instruction) => {
                 self.run(instruction);
-                self.circuit.get(wire)?.ok()
+                self.circuit[wire]?.ok()
             }
         }
     }
@@ -115,7 +118,7 @@ fn main() {
         .lines()
         .map(|line| {
             let parts: Vec<_> = line.split(" -> ").collect();
-            let output = parts[1];
+            let output = Circuit::wire_index(parts[1]);
             let operation = match parts[0] {
                 part if part.contains("AND") => {
                     let parts: Vec<_> = part.splitn(2, " AND ").collect();
@@ -145,17 +148,19 @@ fn main() {
     for instruction in &instructions {
         circuit.run(instruction);
     }
-    println!("Part 1: {}", circuit.get("a").unwrap());
+    let wire_a = Circuit::wire_index("a");
+    println!("Part 1: {}", circuit.get(wire_a).unwrap());
 
     // Part 2
-    let override_b = circuit.get("a").unwrap();
+    let wire_b = Circuit::wire_index("b");
+    let override_b = circuit.get(wire_a).unwrap();
     let mut circuit = Circuit::new();
-    circuit.circuit.insert("b", Ok(override_b));
+    circuit.circuit[wire_b] = Some(Ok(override_b));
     for instruction in &instructions {
-        if instruction.output == "b" {
+        if instruction.output == wire_b {
             continue;
         }
         circuit.run(instruction);
     }
-    println!("Part 2: {}", circuit.get("a").unwrap());
+    println!("Part 2: {}", circuit.get(wire_a).unwrap());
 }
